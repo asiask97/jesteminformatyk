2280db3e720dc116fca53d781ff51f2e9d06143d
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-01-12 16:54:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,52 @@ function process(clicked_id){
     console.log(pickedElement)
     var computerPick = Math.floor((Math.random() * 3) + 1);
     console.log(computerPick)
+
+
    
+    var compscore = document.getElementById('compScore');
+    var yourscore = document.getElementById('yourScore');
+
+
+    var compnumb = compscore.innerHTML;
+    var yournumb = yourscore.innerHTML;    
+    
+    
     if (pickedElement == computerPick){
         alert('tie');
     }
     else if (pickedElement == 1 && computerPick == 2){
-        alert('scizors vs paper -- you won')
+        alert('scizors vs paper -- you won');
+        yournumb++;
+        yourscore.innerHTML = yournumb;
     }
     else if (pickedElement == 1 && computerPick == 3){
-        alert('scizors vs rock -- you lost')
+        alert('scizors vs rock -- you lost');
+        compnumb++;
+        compscore.innerHTML = compnumb;
     }
     else if (pickedElement == 2 && computerPick == 1){
-        alert('paper vs scizors -- you lost')
+        alert('paper vs scizors -- you lost');
+        compnumb++;
+        compscore.innerHTML = compnumb;
     }
     else if (pickedElement == 2 && computerPick == 3){
-        alert('paper vs rock -- you won')
+        alert('paper vs rock -- you won');
+        yournumb++;
+        yourscore.innerHTML = yournumb;
     }
     else if (pickedElement == 3 && computerPick == 1){
-        alert('rock vs scizors -- you won')
+        alert('rock vs scizors -- you won');
+        yournumb++;
+        yourscore.innerHTML = yournumb;
     }
     else if (pickedElement == 3 && computerPick == 2){
-        alert('rock vs paper -- you lost')
+        alert('rock vs paper -- you lost');
+        compnumb++;
+        compscore.innerHTML = compnumb;
     }
     
 
+
+
 }
-
-   
-//function computerspicking(){
-
-   // let computerPick = Math.floor((Math.random() * 3) + 1);
-
-    //if (computerPick === 1){
-    //  var compick = 'scizors';
-   // }
-    //else if(computerPick === 2 ){
-    //    var compick = 'paper';
-   //}
-    //else{
-    //    var compick = 'rock';
-   // }
-
-   // alert(compick);
-
-  // } 
-
-   //computerspicking();
-  
