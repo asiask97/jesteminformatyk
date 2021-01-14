073cb047b23ebe661e272cbdbc0d7186699a7c37
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-01-14 17:03:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,43 +13,65 @@ function process(clicked_id){
 
     var compnumb = compscore.innerHTML;
     var yournumb = yourscore.innerHTML;    
-    
-    
-    if (pickedElement == computerPick){
-        alert('tie');
-    }
-    else if (pickedElement == 1 && computerPick == 2){
-        alert('scizors vs paper -- you won');
-        yournumb++;
-        yourscore.innerHTML = yournumb;
-    }
-    else if (pickedElement == 1 && computerPick == 3){
-        alert('scizors vs rock -- you lost');
-        compnumb++;
-        compscore.innerHTML = compnumb;
-    }
-    else if (pickedElement == 2 && computerPick == 1){
-        alert('paper vs scizors -- you lost');
-        compnumb++;
-        compscore.innerHTML = compnumb;
-    }
-    else if (pickedElement == 2 && computerPick == 3){
-        alert('paper vs rock -- you won');
-        yournumb++;
-        yourscore.innerHTML = yournumb;
-    }
-    else if (pickedElement == 3 && computerPick == 1){
-        alert('rock vs scizors -- you won');
-        yournumb++;
-        yourscore.innerHTML = yournumb;
-    }
-    else if (pickedElement == 3 && computerPick == 2){
-        alert('rock vs paper -- you lost');
-        compnumb++;
-        compscore.innerHTML = compnumb;
-    }
-    
 
+    var suma = compnumb+yournumb;
+
+        
+
+            if (pickedElement == computerPick){
+                alert('tie');
+            }
+            else if (pickedElement == 1 && computerPick == 2){
+                yournumb++;
+                yourscore.innerHTML = yournumb;
+                alert('scizors vs paper -- you won');
+            }
+            else if (pickedElement == 1 && computerPick == 3){
+                compnumb++;
+                compscore.innerHTML = compnumb;
+                alert('scizors vs rock -- you lost');
+            }
+            else if (pickedElement == 2 && computerPick == 1){
+                compnumb++;
+                compscore.innerHTML = compnumb;
+                alert('paper vs scizors -- you lost');
+            }
+            else if (pickedElement == 2 && computerPick == 3){
+                yournumb++;
+                yourscore.innerHTML = yournumb;
+                alert('paper vs rock -- you won');
+            }
+            else if (pickedElement == 3 && computerPick == 1){
+                yournumb++;
+                yourscore.innerHTML = yournumb;
+                alert('rock vs scizors -- you won');
+            }
+            else if (pickedElement == 3 && computerPick == 2){
+                compnumb++;
+                compscore.innerHTML = compnumb;
+                alert('rock vs paper -- you lost');
+            }
+            
+        
+         function gohome(){
+            window.location.href="start.html"
+            }
+
+        var suma = compnumb+yournumb;
+
+            if (compnumb == 3){
+                alert('comp won  -- game will be restarted')
+                gohome();
+               
+            }
+            else if (yournumb == 3){
+                alert(' you won -- game will be restarted')
+                gohome();
+            }
+            
+        
+    
+    
 
 
 }
