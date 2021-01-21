23a1bfcde460a057f5be4b6fdf00a28220bf1571
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-01-21 17:33:33 +0100

    Update draw.js

--- draw.js
+++ draw.js
@@ -14,6 +14,18 @@ for (var i=0; i<2500; i++){
 
 // creating option for more squares
 
+const slider = document.getElementById('slider');
+const root = document.documentElement;
+
+slider.addEventListener('input', (e)=> {
+   // root.style.setProperty("--redius", e.target.value)
+   console.log(e.target.value);
+   
+   var sum = 600/e.target.value;
+   var repeat = "repeat("+ e.target.value +","+ sum + "px)";
+   root.style.setProperty("--idontknow", repeat)
+   console.log(repeat);
+})
 
 
 //36
@@ -27,15 +39,27 @@ for (var i=0; i<2500; i++){
 
 var boxes = document.querySelectorAll('.boxes').forEach(item => {
 
-    item.addEventListener('mouseover', (e)=>{
+    item.addEventListener('mousemove', (e)=>{
      console.log('hi');
      item.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",2000)";
 
      //var boxxes= document.getElementById('box')
      //boxxes.style.backgroundColor='black';
 
-    });
 
+
+// reset button
+        
+    var resetbutton = document.getElementById('reset');
+    resetbutton.addEventListener('click', ()=>{
+        console.log('reseted')
+        item.style.backgroundColor = "white";
+
+})
+})
+
+
+    });
 
 })
 
