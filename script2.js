d2498de326bb7d2ea5fc87e448f771f6a0d31d79
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-01-25 16:28:54 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,10 @@
 var button = document.getElementById('startbutton');
 var buttontwo =document.getElementById('startbutton2');
+var buttonthree = document.getElementById('startbutton3');
 
 buttontwo.addEventListener('mousedown', playAudio);
 button.addEventListener('mousedown', playAudio);
+buttonthree.addEventListener('mousedown', playAudio);
 
 function playAudio() {
     var audio = document.getElementById('audio');
